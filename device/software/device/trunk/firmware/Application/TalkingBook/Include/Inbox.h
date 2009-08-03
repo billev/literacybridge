6c99266aecfd1c3df671a4eaa7a0f3be904dfeff
roger <roger@359e7cc3-0860-1fdf-adbe-24c20723d3c1>
2009-08-03 16:34:12 +0000

    added ability to return processed categories, files, directories from ProcessInbox

--- device/software/device/trunk/firmware/Application/TalkingBook/Include/Inbox.h
+++ device/software/device/trunk/firmware/Application/TalkingBook/Include/Inbox.h
@@ -1,6 +1,13 @@
 #ifndef	__INBOX_h__
 #define	__INBOX_h__
 
-extern unsigned int ProcessInbox(void);
+struct newContent {
+	char newAudioFileCat  [FILE_LENGTH];
+	char newAudioFileName [FILE_LENGTH];
+	char newAudioDirCat   [FILE_LENGTH];
+	char newAudioDirName  [FILE_LENGTH];
+};
+
+extern unsigned int ProcessInbox(struct newContent *);
 
 #endif
