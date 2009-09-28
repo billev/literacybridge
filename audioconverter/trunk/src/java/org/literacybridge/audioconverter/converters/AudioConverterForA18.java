c325ad846cb645ff41cdc1c25f7c0d699f4f81e0
Schade.Sven <Schade.Sven@359e7cc3-0860-1fdf-adbe-24c20723d3c1>
2009-09-28 20:52:54 +0000

    Initial checkin

---
+++ audioconverter/trunk/src/java/org/literacybridge/audioconverter/converters/AudioConverterForA18.java
@@ -0,0 +1,107 @@
+package org.literacybridge.audioconverter.converters;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
+public class AudioConverterForA18 implements IAudioConverter {
+
+	private String converterPath = null;
+	// extracted conversion result
+	private Pattern pattern = Pattern.compile("^Decode\\s'([^']*)'.*");
+	private String resultString;
+	
+	public String getConverterEXEPath() {
+		// converter is stored as an internal resource
+		final URL urlConverterExe = getClass().getResource(
+				"/converters/a18/AudioBatchConverter.exe");
+		if (urlConverterExe == null)
+			return null;
+
+		return urlConverterExe.getPath();
+	}
+	
+	
+	public boolean validateConverterExecutable() 
+	{
+		if (converterPath == null)
+			return false;
+
+		File exeFile = new File(converterPath);
+		if (!exeFile.exists())
+			return false;
+
+		try {
+			boolean success = false;
+			String cmd = converterPath;
+
+			Process proc = Runtime.getRuntime().exec(cmd);
+			InputStream stderr = proc.getInputStream();
+			InputStreamReader isr = new InputStreamReader(stderr);
+			BufferedReader br = new BufferedReader(isr);
+			String line = null;
+
+			while ((line = br.readLine()) != null) {
+				if (line.startsWith("Generalplus Audio Batch Converter Tool")) {
+					success = true;
+				}
+			}
+			proc.waitFor();
+			return success;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	
+	
+	
+	
+	public boolean doConvertFile(File audioFile, File targetDir) throws IOException, InterruptedException {
+		boolean success = false;
+		resultString 	= null;
+
+		String cmd = getConverterEXEPath() 
+						+ " -d "
+						+ "-s 16000 " 
+						+ "-o \"" + targetDir.getAbsolutePath()+ "\" "
+						+ "\"" + audioFile.getAbsolutePath() + "\"";
+
+		Process proc = Runtime.getRuntime().exec(cmd);
+		InputStream stderr = proc.getInputStream();
+		InputStreamReader isr = new InputStreamReader(stderr);
+		BufferedReader br = new BufferedReader(isr);
+		String line = null;
+
+		while ((line = br.readLine()) != null) {
+			Matcher matcher = pattern.matcher(line);
+			if (matcher.matches()) {
+				String fileName = matcher.group(1);
+				if (line.endsWith("Succeeded")) {
+					success = true;
+					resultString = fileName;
+				} else {
+					success = false; // JTBD
+				}
+			}
+		}
+
+		// check for converter error
+		if (success && proc.waitFor() != 0) {
+			resultString = "Converter: Internal error while converting file: '"
+				+ audioFile.getName() + "'";
+		}
+			
+		return success;
+	}
+
+	public String getResult() {
+		return resultString;
+	}
+}
---
+++ audioconverter/trunk/src/java/org/literacybridge/audioconverter/gui/AudioConverter.java
@@ -0,0 +1,475 @@
+package org.literacybridge.audioconverter.gui;
+
+//
+// - Entry point for the java wrapper
+// org.literacybridge.audioconverter.gui.AudioConverter
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.filechooser.FileFilter;
+
+import org.literacybridge.audioconverter.converters.AudioConverterForA18;
+import org.literacybridge.audioconverter.converters.IAudioConverter;
+import org.literacybridge.audioconverter.gui.fileFilters.A18FileFilter;
+import org.literacybridge.audioconverter.gui.fileFilters.WavFileFilter;
+
+public class AudioConverter extends JFrame implements ActionListener,
+		DocumentListener {
+
+	// serial number
+	private static final long serialVersionUID = -1515229785484369683L;
+
+	// buttons
+	JButton sourceButton, targetButton, convertButton, detailsButton;
+	// text fields
+	JTextField sourceDir, targetDir;
+	String sourceDirPath, targetDirPath;
+	// labels
+	JLabel sourceLabel, targetLabel, titleLabel;
+	// panels
+	JComponent mainPanel, detailsPanel;
+	// progress bar
+	JProgressBar progressBar;
+	// details panel is shown
+	boolean detailsVisible = false; // false as default
+	// result text fields
+	JTextArea okFilesTF, badFilesTF;
+	JScrollPane okScrollPane, badScrollPane;
+	
+	StringBuffer okFilesBuffer, badFilesBuffer;
+
+	// convertion
+	File[] files2Convert = null;
+	IAudioConverter currentConverter = null;
+	
+	// converters
+	AudioConverterForA18 a18Converter = null;
+	
+	public AudioConverter() {
+		// initialize JFrame
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Literacy Bridge Audio Converter");
+
+		// load available converters
+		loadConverters();
+		
+		buildControls(false);
+		setLocation(200, 200);
+		setResizable(false);
+		setVisible(true);
+	}
+	
+	private int loadConverters() {
+		int numberOfAvailableConverters = 0;
+		
+		if (a18Converter == null) {
+			a18Converter = new AudioConverterForA18();
+			if (a18Converter.getConverterEXEPath() == null) {
+				JOptionPane.showMessageDialog(
+						this,
+						"Converter could not be loaded for unkown reason!",
+						"A18 Converter problem",
+						JOptionPane.ERROR_MESSAGE, null);
+			}
+		}
+		
+		
+		return numberOfAvailableConverters;
+	}
+	
+	private void buildControls(boolean bShowDetails) {
+
+		getContentPane().removeAll();
+		getContentPane().setLayout(new GridBagLayout());
+
+		// main panel (for the center)	
+		mainPanel = new JPanel(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		// globals
+		int row 	= 0; // current row number
+
+		gbc.insets 	= new Insets(2, 2, 2, 2); // padding
+		gbc.fill 	= GridBagConstraints.BOTH; // resize only horizontal
+		gbc.gridy 	= row;
+			
+		
+		/*
+		 *  title message
+		 */
+		titleLabel = new JLabel("Please enter valid directories.");
+		titleLabel.setHorizontalTextPosition(JLabel.CENTER);
+		titleLabel.setForeground(Color.RED);
+		gbc.gridx 	= 0;
+		gbc.gridwidth = 5;
+		mainPanel.add(titleLabel, gbc);
+		
+		
+		gbc.gridy = ++row;
+		
+		/*
+		 * controls of source directory
+		 */
+		sourceLabel = new JLabel("Source dir: ");
+		gbc.gridx 	= 0;
+		gbc.gridwidth = 1;
+		mainPanel.add(sourceLabel, gbc);
+
+		sourceDir = new JTextField();
+		gbc.gridx 	= 1;
+		gbc.gridwidth = 3;
+		sourceDir.setPreferredSize(new Dimension(300, sourceDir.getHeight()));		 
+		mainPanel.add(sourceDir, gbc);
+		sourceDir.getDocument().addDocumentListener(this);
+		if (sourceDirPath != null) sourceDir.setText(sourceDirPath);
+
+		sourceButton = new JButton("Select...");
+		gbc.gridx 	 = 4;
+		gbc.gridwidth  = 1;
+		mainPanel.add(sourceButton, gbc);
+		sourceButton.addActionListener(this);
+
+		
+		gbc.gridy = ++row;
+		
+		/*
+		 * controls of target directory
+		 */
+		targetLabel = new JLabel("Target dir: ");
+		gbc.gridx 		= 0;
+		gbc.gridwidth	= 1;
+		mainPanel.add(targetLabel, gbc);
+
+		targetDir = new JTextField();
+		targetDir.setPreferredSize(new Dimension(200, 10));
+		gbc.gridx 		= 1;
+		gbc.gridwidth	= 3;		
+		mainPanel.add(targetDir, gbc);
+		targetDir.getDocument().addDocumentListener(this);
+		if (targetDirPath != null) targetDir.setText(targetDirPath);
+
+		targetButton = new JButton("Select...");
+		gbc.gridx = 4;
+		gbc.gridwidth = 1;
+		mainPanel.add(targetButton, gbc);
+		targetButton.addActionListener(this);
+
+		
+		gbc.gridy = ++row;
+		
+		/*
+		 * progress bar 
+		 */
+		progressBar = new JProgressBar();
+		progressBar.setStringPainted(true);
+		gbc.gridx 	= 0;
+		gbc.gridwidth = 5;
+		mainPanel.add(progressBar, gbc);
+
+		
+		gbc.gridy = ++row;
+		
+		/*
+		 * buttons 
+		 */
+		
+		detailsButton = new JButton("Details");
+		detailsButton.setEnabled(true);
+		gbc.gridx 	= 0;
+		gbc.gridwidth = 1;
+		mainPanel.add(detailsButton, gbc);
+		
+		detailsButton.addActionListener(this);
+		
+		convertButton = new JButton("Convert");
+		convertButton.addActionListener(this);
+		convertButton.setEnabled(false);
+		gbc.gridx 	= 4;
+		gbc.gridwidth = 1;
+		mainPanel.add(convertButton, gbc);
+
+		
+		// Show details at bottom of JFrame
+		if (bShowDetails) {
+			buildDetailsPanel();
+		}
+					
+		// add Panels to parent frame
+		organizePanels();
+	}
+	
+	private void organizePanels() {
+
+		// add main panel to parent
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill 	= GridBagConstraints.HORIZONTAL; // resize only horizontal
+		gbc.gridx = 0; 
+		gbc.gridy = 0;
+		gbc.weightx = 0;
+		gbc.weighty = 0;
+		getContentPane().add(mainPanel, gbc);
+		
+		// add detail panel to parent
+		if (detailsVisible) {
+			gbc.gridx = 0; 
+			gbc.gridy = 1;
+			gbc.weighty = 1.0;
+			gbc.fill 	= GridBagConstraints.BOTH; // resize
+			getContentPane().add(detailsPanel, gbc);
+		}
+
+		pack();	
+	}
+	
+	private void buildDetailsPanel() {
+		detailsPanel = new JPanel(new GridBagLayout());
+		detailsPanel.setBorder(new TitledBorder("Conversion Status"));
+
+		GridBagConstraints gbc = new GridBagConstraints();
+		int row 	= 0; // current row number
+		gbc.gridx 	= 0;
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
+		gbc.fill 	= GridBagConstraints.BOTH;
+		gbc.insets  = new Insets(2, 2, 2, 2); 
+
+		JLabel okLabel = new JLabel("Success:");
+		gbc.gridy 	= row;
+		detailsPanel.add(okLabel, gbc);
+		
+		++row;
+		
+		okFilesTF	= new JTextArea(10, 10);
+		okFilesTF.setEditable(false);
+		gbc.gridy 	= row;
+		okScrollPane = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		okScrollPane.getViewport().setView(okFilesTF);
+		detailsPanel.add(okScrollPane, gbc);
+		if (okFilesBuffer != null && okFilesBuffer.length() != 0) {
+			okFilesTF.append(okFilesBuffer.toString());
+		}
+		
+		++row;
+		
+		JLabel failedLabel = new JLabel("Failed:");
+		gbc.gridy 	= row;
+		detailsPanel.add(failedLabel, gbc);
+		
+		++row;
+		
+		badFilesTF	= new JTextArea(10, 10);
+		badFilesTF.setEditable(false);
+		gbc.gridy = row;
+		badScrollPane = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		badScrollPane.getViewport().setView(badFilesTF);
+		detailsPanel.add(badScrollPane, gbc);
+		if (badFilesBuffer != null && badFilesBuffer.length() != 0) {
+			badFilesTF.append(badFilesBuffer.toString());
+		}
+	}
+	
+	
+	public void actionPerformed(ActionEvent e) {
+
+		// Handle open button action.
+		if (e.getSource() == sourceButton) {
+			JFileChooser fc = null;
+			if (sourceDirPath != null) fc = new JFileChooser(sourceDirPath);
+			else if (this.sourceDir.getText().length() != 0) fc = new JFileChooser(this.sourceDir.getText());
+			else fc = new JFileChooser();
+			
+			fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+			fc.setMultiSelectionEnabled(true);
+			fc.setDialogTitle("Select source directory...");
+			// file filter
+			FileFilter a18 = new A18FileFilter(true);
+			fc.addChoosableFileFilter(a18);
+			fc.setFileFilter(a18);
+			
+			
+			int returnVal = fc.showOpenDialog(AudioConverter.this);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				files2Convert = fc.getSelectedFiles();
+				sourceDirPath = files2Convert[0].getParent();
+				this.sourceDir.setText(sourceDirPath); 
+			}
+	
+		} else if (e.getSource() == targetButton) {
+			JFileChooser fc = null;
+			if (targetDirPath != null) fc = new JFileChooser(targetDirPath);
+			else if (this.targetDir.getText().length() != 0) fc = new JFileChooser(this.targetDir.getText());
+			else fc = new JFileChooser();
+
+			fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			fc.setDialogTitle("Select target directory...");
+			// file filter
+			FileFilter wav = new WavFileFilter(false);
+			fc.addChoosableFileFilter(wav);
+			fc.setFileFilter(wav);
+			fc.setAcceptAllFileFilterUsed(false);
+			
+			int returnVal = fc.showSaveDialog(AudioConverter.this);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				File file = fc.getSelectedFile();
+				targetDirPath = file.getAbsolutePath();
+				this.targetDir.setText(targetDirPath);
+				FileFilter ff = fc.getFileFilter();
+				if (ff instanceof WavFileFilter) {
+					currentConverter = a18Converter;				
+				}
+			}
+		} else if (e.getSource() == detailsButton) {
+				
+			if (detailsVisible) {
+				detailsVisible = false;
+				buildControls(false);
+			} else {
+				detailsVisible = true;
+				buildControls(true);
+			}
+			
+		} else if (e.getSource() == convertButton) {
+			
+			// Check if convert is there
+			if (currentConverter != null && files2Convert.length > 0) {
+					
+				//  progress bar
+				progressBar.setStringPainted(true);
+				progressBar.setMaximum(files2Convert.length);
+				progressBar.setValue(0);
+				
+				if (detailsVisible) {
+					okFilesTF.setText("");
+					badFilesTF.setText("");
+				}
+				
+				okFilesBuffer = new StringBuffer();
+				badFilesBuffer = new StringBuffer();
+				
+				final File targetDirPath = new File(targetDir.getText());
+
+				// run conversion in separate thread to avoid that the GUI
+				// freezes
+				Runnable runnable = new Runnable() {
+					public void run() {
+						for (int i = 0; i < files2Convert.length; ++i) {
+							File currFile = files2Convert[i];
+							try {
+								if (currentConverter.doConvertFile(currFile, targetDirPath)) {
+									if (detailsVisible) {
+										okFilesTF.append(currFile.getName() + "\n");
+									}
+									okFilesBuffer.append(currFile.getName()	+ "\n");
+								} else {
+									if (detailsVisible) {
+										badFilesTF.append(currFile.getName() + "\n");
+									}
+									badFilesBuffer.append(currFile.getName() + "\n");
+								}
+
+							} catch (Exception ex) {
+								ex.printStackTrace();
+							}
+							// update progress bar
+							progressBar.setValue(i + 1);
+						}
+					}
+				};
+
+				// start conversion
+				Thread t = new Thread(runnable);
+				t.start();
+			}
+			else
+			{
+				JOptionPane.showMessageDialog(this, "Internal Error: No converter set",
+				"Converter Error", JOptionPane.ERROR_MESSAGE,
+				null);
+				return;
+			}
+		}
+
+		validateDirs();
+	}
+
+	private void validateDirs() {
+		if (sourceDir.getText().trim().length() == 0 && targetDir.getText().trim().length() == 0) {
+			message("Please enter valid directories.");
+			return;
+		} else {
+			message("");
+		}
+
+		boolean e = false;
+		File source = new File(sourceDir.getText());
+		if (!source.exists()) {
+			message("Source does not exist.");
+		} else if (!source.isDirectory()) {
+			message("Source is not a directory.");
+		} else if (targetDir.getText().trim().length() == 0) {
+			message("Please enter a valid target directory");
+		} else {
+			File target = new File(targetDir.getText());
+			if (!target.exists()) {
+				message("Target does not exist.");
+			} else if (!target.isDirectory()) {
+				message("Target ist not a directory.");
+			} else {
+				e = true;
+			}
+		}
+
+		convertButton.setEnabled(e);
+	}
+
+	private void message(String text) {
+		this.titleLabel.setText(text);
+	}
+
+
+	public void changedUpdate(DocumentEvent e) {
+		// NOOP
+	}
+
+	public void insertUpdate(DocumentEvent e) {
+		validateDirs();
+	}
+
+	public void removeUpdate(DocumentEvent e) {
+		validateDirs();
+	}
+
+	public static void main(String[] args) {
+		// Schedule a job for the event dispatch thread:
+		// creating and showing this application's GUI.
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				// Turn off metal's use of bold fonts
+				UIManager.put("swing.boldMetal", Boolean.FALSE);
+				new AudioConverter();
+			}
+		});
+	}
+}
