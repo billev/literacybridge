e7108d2974707a6ffcbc7f233e9b3ed501f6e13a
cliff <cliff@359e7cc3-0860-1fdf-adbe-24c20723d3c1>
2010-05-07 19:28:32 +0000

    creating stable branch for version 1.40 release to customers

---
+++ device/software/device/branches/v1.40-release/firmware/Application/TalkingBook/Include/sys_counters.h
@@ -0,0 +1,23 @@
+// Copyright 2009 Literacy Bridge
+// CONFIDENTIAL -- Do not share without Literacy Bridge Non-Disclosure Agreement
+// Contact: info@literacybridge.org
+#ifndef	__SYS_COUNTERS_h__
+#define	__SYS_COUNTERS_h__
+
+typedef struct SystemCounts SystemCounts;
+
+struct SystemCounts {
+	unsigned long powerUpNumber;
+	unsigned long packageNumber;
+	unsigned long listNumber;
+	unsigned long lastLogErase;
+	unsigned long revdPkgNumber;
+};
+
+extern SystemCounts systemCounts;
+
+extern void saveSystemCounts(void);
+extern int loadSystemCounts(void);
+extern void getPkgNumber(char *, BOOL);
+extern void getrevdPkgNumber(char *, BOOL);
+#endif
