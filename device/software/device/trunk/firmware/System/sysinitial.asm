768afce09b6994296e26aed500c1589542ad7a6a
roger <roger@359e7cc3-0860-1fdf-adbe-24c20723d3c1>
2010-06-25 19:15:20 +0000

    TB_CAN_WAIT removed from .asm files, added extern in audio.c and mainLoop.c

--- device/software/device/trunk/firmware/System/sysinitial.asm
+++ device/software/device/trunk/firmware/System/sysinitial.asm
@@ -5,11 +5,17 @@
 
 .PUBLIC _Sys_Initial
 _Sys_Initial: .PROC
-.ifdef TB_CAN_WAKE
+//.ifdef TB_CAN_WAKE
+	r1 = _MEM_TYPE
+	cmp  r1, MX_MID
+	jnz oldbd
 	r1=0x8618
-.else
+	jmp oldbd1
+//.else
+_oldbd:
 	r1=0x8418
-.endif
+//.endif
+_oldbd1:
 	[P_Clock_Ctrl]=r1	//pll enable, system clock=PLL clock
 
 	r1=0x20				//PLL clock =96MHz
