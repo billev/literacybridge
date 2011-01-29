6dd14338445fcdff105f71a9a5812882cbf9ac99
sadsaddle@gmail.com <sadsaddle@gmail.com@359e7cc3-0860-1fdf-adbe-24c20723d3c1>
2011-01-29 23:58:08 +0000

    add meta data constants from acm

--- device/software/device/trunk/firmware/Application/TalkingBook/Include/metadata.h
+++ device/software/device/trunk/firmware/Application/TalkingBook/Include/metadata.h
@@ -3,10 +3,32 @@
 
 // added for meta data 
 #define CURRENT_POS 0xffffffffL
-#define DC_IDENTIFIER 10 
-#define DC_LANGUAGE   12
-#define DC_CATEGORY    0
-#define DC_AUDIO_ITEM_ID 0x40   // change to match ACM
+//from acm 
+#define DC_CATEGORY			0
+#define DC_TITLE			1
+#define DC_CREATOR			2
+#define DC_SUBJECT			3
+#define DC_DESCRIPTION		4
+#define DC_PUBLISHER		5
+#define DC_CONTRIBUTOR		6
+#define DC_DATE				7
+#define DC_TYPE				8
+#define DC_FORMAT			9
+#define DC_IDENTIFIER		10
+#define DC_SOURCE			11
+#define DC_LANGUAGE			12
+#define DC_RELATION			13
+#define DC_COVERAGE			14
+#define DC_RIGHTS			15
+#define DTB_REVISION		16
+#define DTB_REVISION_DATE	17
+#define DTB_REVISION_DESCRIPTION	18
+#define LB_COPY_COUNT		19
+#define LB_PLAY_COUNT		20
+#define LB_RATING			21
+
+#define DC_AUDIO_ITEM_ID	0x40   // change to match ACM
+
 #define METADATA_VERSION 1
 #define META_CURRENT_VERSION 1
 
