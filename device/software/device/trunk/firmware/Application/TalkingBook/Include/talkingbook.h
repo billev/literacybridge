c68f5a3c32e45b85c228a3fc24545408f659b3e1
cliff <cliff@literacybridge.org>
2011-02-03 21:00:45 +0000

    The system langauge code is found in pkgSystem.idxLanguage.  It used to be set from a field in the control.txt file, but now it is set to be the same as the package name (.idxName) for system packages, which just comes from the name of the directory.  In the future, we may parse the name to separate the true language code from an appendix that indicates a profile for that language (such as Admin Mode in English vs. regular user English).

--- device/software/device/trunk/firmware/Application/TalkingBook/Include/talkingbook.h
+++ device/software/device/trunk/firmware/Application/TalkingBook/Include/talkingbook.h
@@ -4,7 +4,7 @@
 #ifndef	__TALKINGBOOK_h__
 #define	__TALKINGBOOK_h__
 
-#define VERSION			"v1.50r572"
+#define VERSION			"v1.50r573"
 #define TB_CAN_WAKE  // hardware that allows wake from sleep/halt
 //IMPORTANT: changes to TB_CAN_WAKE must also be made to system_head.inc!!!
 
