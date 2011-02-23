579b4aaae514b132f50d3246ab19ae2ad2f68647
mahashin <mahashin@gmail.com>
2011-02-23 06:53:54 +0000

    creating branch for language creation feature

---
+++ device/software/device/branches/create-lang/firmware/Driver/Include/driver_head.h
@@ -0,0 +1,18 @@
+//========================================================================================
+//Header Name:		Graphics.h
+//Applied body:		SPL16256A
+//Purpose:		Header File for the C language that try to call functions of the graphics library
+//Programmer:		CP2
+//Written Date:		2003-10-1
+//Version history:
+//========================================================================================
+#ifndef	__DRVIVER_HEAD_H__
+#define	__DRVIVER_HEAD_H__
+//SD
+#include ".\driver\include\SD\SDCDriver.h"
+
+//Nand
+#include ".\driver\include\NandDriver\Nand2_Oper.h"
+#include ".\driver\include\NandDriver\nanddriver.h"
+
+#endif
---
+++ device/software/device/branches/create-lang/firmware/System/SysException.c
@@ -0,0 +1,4 @@
+void Sys_Exception(unsigned int errorCode) {
+	// errorcode == 1 means memory error from BodyInit() and ucBSInit()
+	while(1);
+}
