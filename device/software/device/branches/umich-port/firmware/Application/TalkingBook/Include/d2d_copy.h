93c94f0f749ec36cc89b2e0b145cc72cf7bd6927
ddevec <ddevec@umich.edu>
2011-03-11 21:27:16 +0000

    removing umich-port branch of literacy bridge, accidently branched wrong revision

--- device/software/device/branches/umich-port/firmware/Application/TalkingBook/Include/d2d_copy.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2009 Literacy Bridge
-// CONFIDENTIAL -- Do not share without Literacy Bridge Non-Disclosure Agreement
-// Contact: info@literacybridge.org
-#ifndef	__D2D_COPY_h__
-#define	__D2D_COPY_h__
-
-extern int d2dCopy(const char *, const char *);
-
-#endif
