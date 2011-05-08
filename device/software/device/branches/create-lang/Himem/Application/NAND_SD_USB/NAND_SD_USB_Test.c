093d3c95bf382031b9b0512d7807397aa13e5d63
cliff@literacybridge.org <cliff@literacybridge.org@359e7cc3-0860-1fdf-adbe-24c20723d3c1>
2011-05-08 00:47:27 +0000

    closing down this branch.  Feature work was done and merged into trunk.

--- device/software/device/branches/create-lang/Himem/Application/NAND_SD_USB/NAND_SD_USB_Test.c
+++
@@ -1,9 +0,0 @@
-#include "./system/include/system_head.h"
-#include "./driver/include/driver_head.h"
-#include "./component/include/component_head.h"
-
-int SystemIntoUDisk();
-void Nand_SD_USB_Test(void)
-{
-	SystemIntoUDisk();			//test usb
-}
--- device/software/device/branches/create-lang/Himem/Debug/higet.bat
+++
@@ -1,2 +0,0 @@
-"tools\srec2bin" -q debug\System.S37 debug\System.bin
-dd if=himem.bin of=himem.blk bs=32k count=2 skip=40
