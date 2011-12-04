cbcc1c31d5a1ab20a9d42be1510d05c27f7c00a2
cliffschmidt <cliffschmidt@gmail.com>
2011-12-04 00:17:39 +0000

    tag for release used in Oct 2011 deployment of maternal and child health program in Upper West, Ghana.

---
+++ device/software/device/tags/v1.51r793a/firmware/Application/TalkingBook/Include/Inbox.h
@@ -0,0 +1,12 @@
+// Copyright 2009 Literacy Bridge
+// CONFIDENTIAL -- Do not share without Literacy Bridge Non-Disclosure Agreement
+// Contact: info@literacybridge.org
+#ifndef	__INBOX_h__
+#define	__INBOX_h__
+
+#define 	BAD_IMPORTS_SUBDIR	(char *)"bad-imports/"
+
+extern void processInbox(void);
+extern int metaRead(int, unsigned int, unsigned int *);
+
+#endif
---
+++ device/software/device/tags/v1.51r793a/firmware/Application/TalkingBook/Include/files.h
@@ -0,0 +1,44 @@
+// Copyright 2009 Literacy Bridge
+// CONFIDENTIAL -- Do not share without Literacy Bridge Non-Disclosure Agreement
+// Contact: info@literacybridge.org
+#ifndef	__FILES_h__
+#define	__FILES_h__
+
+#include ".\Component\Include\FS\vfs.h"
+#include "./Application/TalkingBook/Include/lists.h"
+
+#define LOG_BUFFER_SIZE		512
+#define COPY_BUFFER_SIZE    1024
+#define ASAP				0
+#define BUFFER				1
+
+extern void logString(char *, int);
+extern char *getLine (int, char *);
+extern BOOL readBuffer(int, char *, int);
+extern BOOL nextNameValuePair (int, char *, char, char **, char **);
+extern void clearStaleLog(void);
+extern void flushLog(void);
+extern int appendStringToFile(const char *, char *);
+extern int insertStringInFile(const char *, char *, long);
+extern int insertStringInNewFile (const char *, char *);
+//extern int createControlFromTemplate(char *, char *);
+extern int findDeleteStringFromFile(char *, char *, const char *, BOOL);
+extern long getFilePosition(void);
+extern INT16 tbOpen(LPSTR, INT16);
+extern INT16 tbChdir(LPSTR);
+extern int fileExists(LPSTR);
+extern int dirExists(LPSTR);
+extern int convertDoubleToSingleChar(char *, const char *, BOOL);
+extern void trimFile(char *, unsigned long, unsigned long);
+extern int fileCopy(char *, char *);
+extern void dirCopy(char *, char *, BOOL);
+extern int copyMovedir(char *, char *);
+extern void copyAllFiles(char *, char *, BOOL);
+extern void moveAllFiles(char *, char *);
+extern void deleteAllFiles(char *);
+extern void moveAudioFiles(char *, char *);
+extern void categoryStringtoLong(char *, MLENTRY *);
+extern void categoryLongtoString(char *, MLENTRY *);
+extern unsigned int loadLanglisttoMemory(char *,  MLENTRY[], unsigned int);
+
+#endif
