13bb9a9ea9fb7e3b3b2fb88d15d788064a9a13c4
cliffschmidt@gmail.com <cliffschmidt@gmail.com@359e7cc3-0860-1fdf-adbe-24c20723d3c1>
2012-03-08 20:39:52 +0000

    bug fix: device-58 code (inspecting for category list name locked with '!') in getNextList() caused the cursor ptr to move, which caused misalignment of list->currentFilePosition.  So I've moved setting of currentFilePositon before the device-58 lines.

--- device/software/device/trunk/firmware/Application/TalkingBook/Include/talkingbook.h
+++ device/software/device/trunk/firmware/Application/TalkingBook/Include/talkingbook.h
@@ -4,7 +4,7 @@
 #define	__TALKINGBOOK_h__
 
 #define MAJOR_VERSION	"v1.55"
-#define SVN_REVISION	"r813"
+#define SVN_REVISION	"r815"
 #define VERSION			MAJOR_VERSION SVN_REVISION
 #define TB_CAN_WAKE  // hardware that allows wake from sleep/halt
 //IMPORTANT: changes to TB_CAN_WAKE must also be made to system_head.inc!!!
